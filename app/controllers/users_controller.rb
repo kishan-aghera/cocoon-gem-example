453526ad741afd20bdae7d51f254a069d19c6544
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-04-27 17:22:38 +0530

    Configured Users controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    @friends = @user.friends.build
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, friends_attributes: [:id, :friend_id, :friend_name, :_destroy])
   end
 end
